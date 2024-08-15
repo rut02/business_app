56658f9a1f4c5484ada9e46c179ec48d2af0993b
Your Name <you@example.com>
2024-08-16 00:58:54 +0700

    สฟหะ

--- lib/screens/account.dart
+++ lib/screens/account.dart
@@ -40,156 +40,188 @@ class _AccountScreenState extends State<AccountScreen> {
     });
   }
 
-  Future<void> _showChangeProfileImageDialog() async {
-    final userId = Provider.of<LoginProvider>(context, listen: false).login?.id;
-    final image = await _picker.pickImage(source: ImageSource.gallery);
+  // Future<void> _showChangeProfileImageDialog() async {
+  //   final userId = Provider.of<LoginProvider>(context, listen: false).login?.id;
+  //   final image = await _picker.pickImage(source: ImageSource.gallery);
 
-    if (image != null && userId != null) {
-      final result = await UserService().uploadProfileImage(userId, 'profile', image.path);
-      if (result != null) {
-        _refreshUserData();
-      }
-    }
-  }
+  //   if (image != null && userId != null) {
+  //     final result = await UserService().uploadProfileImage(userId, 'profile', image.path);
+  //     if (result != null) {
+  //       _refreshUserData();
+  //     }
+  //   }
+  // }
 
   
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder<User>(
-      future: _userFuture,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Scaffold(
-            appBar: AppBar(
-              title: Text('บัญชี'),
-            ),
-            body: Center(
-              child: CircularProgressIndicator(),
-            ),
-          );
-        } else if (snapshot.hasError) {
-          return Scaffold(
-            appBar: AppBar(
-              title: Text('บัญชี'),
-            ),
-            body: Center(
-              child: Text('ข้อผิดพลาด: ${snapshot.error}'),
-            ),
-          );
-        } else if (!snapshot.hasData) {
-          return Scaffold(
-            appBar: AppBar(
-              title: Text('บัญชี'),
-            ),
-            body: Center(
-              child: Text('ไม่พบข้อมูลบัญชี'),
-            ),
-          );
-        } else {
-          final user = snapshot.data!;
-          return Scaffold(
-            appBar: AppBar(
-              title: Text('บัญชี'),
-            ),
-            body: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  ListTile(
-                    leading: GestureDetector(
-                      onTap: _showChangeProfileImageDialog,
-                      child: CircleAvatar(
-                        radius: 30,
-                        backgroundImage: user.profile.isNotEmpty
-                            ? NetworkImage(user.profile)
-                            : null,
-                        child: user.profile.isEmpty
-                            ? Icon(Icons.account_circle, size: 30)
-                            : null,
-                      ),
-                    ),
-                    title: Text('${user.firstname} ${user.lastname}'),
-                    subtitle: Text(user.email),
-                  ),
-                  Divider(),
-                  ListTile(
-                    leading: Icon(Icons.edit),
-                    title: Text('แก้ไขรายละเอียดบัญชี'),
-                    onTap: () async {
-                      final result = await Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => EditAccountScreen(user: user),
-                        ),
-                      );
-                      if (result == true) {
-                        _refreshUserData();
-                      }
-                    },
-                  ),
-                  Divider(),
-                  ListTile(
-                    leading: Icon(Icons.lock),
-                    title: Text('เปลี่ยนรหัสผ่าน'),
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => ChangePasswordScreen()),
-                      );
-                    },
-                  ),
-                  Divider(),
-                  ListTile(
-                    leading: Icon(Icons.logout),
-                    title: Text('ออกจากระบบ'),
-                    onTap: () {
-                      // แสดงกล่องโต้ตอบยืนยันก่อนออกจากระบบ
-                      showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return AlertDialog(
-                            title: Text('ออกจากระบบ'),
-                            content: Text('คุณแน่ใจหรือไม่ที่จะออกจากระบบ?'),
-                            actions: <Widget>[
-                              TextButton(
-                                child: Text('ยกเลิก'),
-                                onPressed: () {
-                                  Navigator.of(context).pop(); // ปิดกล่องโต้ตอบ
-                                },
-                              ),
-                              TextButton(
-                                child: Text('ออกจากระบบ'),
-                                onPressed: () {
-                                  // ลงชื่อออกที่นี่
-                                  Provider.of<LoginProvider>(context,
-                                          listen: false)
-                                      .logout();
+  void _showChangeProfileImageDialog() {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text('เปลี่ยนรูปโปรไฟล์'),
+        content: Text('คุณต้องการเปลี่ยนรูปโปรไฟล์หรือไม่?'),
+        actions: <Widget>[
+          TextButton(
+            child: Text('ยกเลิก'),
+            onPressed: () {
+              Navigator.of(context).pop(); // ปิดกล่องโต้ตอบ
+            },
+          ),
+          TextButton(
+            child: Text('เลือกจากแกลเลอรี่'),
+            onPressed: () async {
+              Navigator.of(context).pop(); // ปิดกล่องโต้ตอบ
+              final userId = Provider.of<LoginProvider>(context, listen: false).login?.id;
+              final image = await _picker.pickImage(source: ImageSource.gallery);
 
-                                  // นำทางไปยังหน้าลงชื่อเข้าใช้
-                                  Navigator.pushAndRemoveUntil(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) => LoginScreen(),
-                                    ),
-                                    (Route<dynamic> route) => false,
-                                  );
-                                },
-                              ),
-                            ],
-                          );
-                        },
-                      );
-                    },
+              if (image != null && userId != null) {
+                final result = await UserService().uploadProfileImage(userId, 'profile', image.path);
+                if (result != null) {
+                  _refreshUserData();
+                }
+              }
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
+
+@override
+Widget build(BuildContext context) {
+  return FutureBuilder<User>(
+    future: _userFuture,
+    builder: (context, snapshot) {
+      if (snapshot.connectionState == ConnectionState.waiting) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text('บัญชี'),
+          ),
+          body: Center(
+            child: CircularProgressIndicator(),
+          ),
+        );
+      } else if (snapshot.hasError) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text('บัญชี'),
+          ),
+          body: Center(
+            child: Text('ข้อผิดพลาด: ${snapshot.error}'),
+          ),
+        );
+      } else if (!snapshot.hasData) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text('บัญชี'),
+          ),
+          body: Center(
+            child: Text('ไม่พบข้อมูลบัญชี'),
+          ),
+        );
+      } else {
+        final user = snapshot.data!;
+        return Scaffold(
+          appBar: AppBar(
+            title: Text('บัญชี'),
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ListTile(
+                  leading: GestureDetector(
+                    onTap: _showChangeProfileImageDialog,
+                    child: CircleAvatar(
+                      radius: 30,
+                      backgroundImage: user.profile.isNotEmpty
+                          ? NetworkImage(user.profile)
+                          : null,
+                      child: user.profile.isEmpty
+                          ? Icon(Icons.account_circle, size: 30)
+                          : null,
+                    ),
                   ),
-                  Divider(),
-                ],
-              ),
+                  title: Text('${user.firstname} ${user.lastname}'),
+                  subtitle: Text(user.email),
+                ),
+                Divider(),
+                ListTile(
+                  leading: Icon(Icons.edit),
+                  title: Text('แก้ไขรายละเอียดบัญชี'),
+                  onTap: () async {
+                    final result = await Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => EditAccountScreen(user: user),
+                      ),
+                    );
+                    if (result == true) {
+                      _refreshUserData();
+                    }
+                  },
+                ),
+                Divider(),
+                ListTile(
+                  leading: Icon(Icons.lock),
+                  title: Text('เปลี่ยนรหัสผ่าน'),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => ChangePasswordScreen()),
+                    );
+                  },
+                ),
+                Divider(),
+                ListTile(
+                  leading: Icon(Icons.logout),
+                  title: Text('ออกจากระบบ'),
+                  onTap: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: Text('ออกจากระบบ'),
+                          content: Text('คุณแน่ใจหรือไม่ที่จะออกจากระบบ?'),
+                          actions: <Widget>[
+                            TextButton(
+                              child: Text('ยกเลิก'),
+                              onPressed: () {
+                                Navigator.of(context).pop(); // ปิดกล่องโต้ตอบ
+                              },
+                            ),
+                            TextButton(
+                              child: Text('ออกจากระบบ'),
+                              onPressed: () {
+                                Provider.of<LoginProvider>(context,
+                                        listen: false)
+                                    .logout();
+                                Navigator.pushAndRemoveUntil(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => LoginScreen(),
+                                  ),
+                                  (Route<dynamic> route) => false,
+                                );
+                              },
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
+                ),
+                Divider(),
+              ],
             ),
-          );
-        }
-      },
-    );
-  }
+          ),
+        );
+      }
+    },
+  );
+}
+
 }
